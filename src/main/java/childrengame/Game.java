cd0bcc92d48eb03e5e8df58c51b91a849a41dd5d
Mohamed Suwaibith Suhail <twistergillaz@gmail.com>
2019-02-12 09:55:55 +1100

    Updated to use String instead of ArrayList hence being more meaningful.

--- src/main/java/childrengame/Game.java
+++ src/main/java/childrengame/Game.java
@@ -30,6 +30,7 @@ import java.util.stream.IntStream;
 public class Game {
 
     private static final int ZERO = 0;
+    private static final String EMPTY_SPACE = " ";
     private static final Logger logger = LoggerFactory.getLogger(Game.class);
 
     /**
@@ -38,50 +39,39 @@ public class Game {
      *
      * @param numOfChildren number of children that are participating in the game.
      * @param removedAt     the position of the child in the circle to be removed from the game.
-     * @return an array list which contains the order of the sequence of the children eliminated from the game and winner who is
-     * represented as the last element in the array list.
+     * @return resultsOfGame a String which contains the order of the sequence of the children eliminated from the game and winner who is
+     * represented as the last element.
      * @throws InvalidChildrenGameInputException when the number of children or the constant(removeAt) is less than or equal
      *                                           to zero.
      */
-    public static ArrayList<Integer> printSequenceAndWinner(int numOfChildren, int removedAt) throws InvalidChildrenGameInputException {
+    public static String printSequenceAndWinner(int numOfChildren, int removedAt) throws InvalidChildrenGameInputException {
         if (numOfChildren > 0 && removedAt > 0) {
             ArrayList<Integer> childrenArray = new ArrayList<>(numOfChildren);
-            ArrayList<Integer> resultOfGame = new ArrayList<>();
+            StringBuilder resultsOfGame = new StringBuilder("Removed:");
 
             childrenArray.addAll(IntStream.rangeClosed(1, numOfChildren).boxed().collect(Collectors.toList()));
-            removeChild(childrenArray, resultOfGame, ZERO, removedAt);
-            return resultOfGame;
+            removeChild(childrenArray, resultsOfGame, ZERO, removedAt);
+            return resultsOfGame.toString();
         } else {
             throw new InvalidChildrenGameInputException(numOfChildren, removedAt);
         }
     }
 
     /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        try {
-            printSequenceAndWinner(5, 1);
-        } catch (InvalidChildrenGameInputException ex) {
-            logger.error("Invalid number of children '{}' or '{}'", ex.getNumberOfChildren(), ex.getRemovedAt());
-        }
-    }
-
-    /**
      * This is a recursive function that continues to execute until the all the children in the circle has been eliminated except
-     * for the winner. The winner is the last element that is added to the resultOfGame ArrayList.
+     * for the winner. The winner is the last element that is concatenated to the resultOfGame.
      *
      * @param childrenInCircle is an array list that contains the list of children's Ids sequentially.
-     * @param resultOfGame     is an array list of all children removed from the game and the winner which is represented as the last element.
+     * @param resultsOfGame    is a String of all children removed from the game and the winner which is represented as the last element.
      * @param startingIndex    represents the position of the student in the circle.
      * @param removeAt         the position of the child in the circle to be removed from the game.
      * @return returns the next child's position after elimination.
      */
-    private static int removeChild(ArrayList<Integer> childrenInCircle, ArrayList<Integer> resultOfGame, int startingIndex, int removeAt) {
+    private static int removeChild(ArrayList<Integer> childrenInCircle, StringBuilder resultsOfGame, int startingIndex, int removeAt) {
         if (childrenInCircle.size() == 1) {
             int winner = childrenInCircle.remove(ZERO);
             logger.info("Winner: {}", winner);
-            resultOfGame.add(winner);
+            resultsOfGame.append(", Winner: ").append(winner);
             return winner;
         } else {
             for (int count = 1, childIndex = startingIndex; count <= removeAt; count++, childIndex++) {
@@ -92,13 +82,13 @@ public class Game {
                 if (count == removeAt) {
                     int removedChildId = childrenInCircle.remove(childIndex);
                     logger.info("Removed: {}", removedChildId);
-                    resultOfGame.add(removedChildId);
+                    resultsOfGame.append(EMPTY_SPACE).append(removedChildId);
                     if (childIndex < childrenInCircle.size()) {
-                        return removeChild(childrenInCircle, resultOfGame, childIndex, removeAt);
+                        return removeChild(childrenInCircle, resultsOfGame, childIndex, removeAt);
                     }
                 }
             }
-            return removeChild(childrenInCircle, resultOfGame, ZERO, removeAt);
+            return removeChild(childrenInCircle, resultsOfGame, ZERO, removeAt);
         }
     }
 
--- src/test/java/childrengame/GameTests.java
+++ src/test/java/childrengame/GameTests.java
@@ -20,44 +20,43 @@ public class GameTests {
     public Object[][] getData() {
         return new Object[][]{
 
-                {new TestDataForGame(new int[]{1, 2, 3, 4}, 4, 1)},
-                {new TestDataForGame(new int[]{2, 4, 3, 1}, 4, 2)},
-                {new TestDataForGame(new int[]{3, 2, 4, 1}, 4, 3)},
-                {new TestDataForGame(new int[]{4, 1, 3, 2}, 4, 4)},
-                {new TestDataForGame(new int[]{1, 3, 4, 2}, 4, 5)},
-                {new TestDataForGame(new int[]{2, 1, 4, 3}, 4, 6)},
-                {new TestDataForGame(new int[]{3, 4, 1, 2}, 4, 7)},
-                {new TestDataForGame(new int[]{4, 2, 1, 3}, 4, 8)},
+                {new TestDataForGame("Removed: 1 2 3, Winner: 4", 4, 1)},
+                {new TestDataForGame("Removed: 2 4 3, Winner: 1", 4, 2)},
+                {new TestDataForGame("Removed: 3 2 4, Winner: 1", 4, 3)},
+                {new TestDataForGame("Removed: 4 1 3, Winner: 2", 4, 4)},
+                {new TestDataForGame("Removed: 1 3 4, Winner: 2", 4, 5)},
+                {new TestDataForGame("Removed: 2 1 4, Winner: 3", 4, 6)},
+                {new TestDataForGame("Removed: 3 4 1, Winner: 2", 4, 7)},
+                {new TestDataForGame("Removed: 4 2 1, Winner: 3", 4, 8)},
 
-                {new TestDataForGame(new int[]{1, 2, 3, 4, 5}, 5, 1)},
-                {new TestDataForGame(new int[]{2, 4, 1, 5, 3}, 5, 2)},
-                {new TestDataForGame(new int[]{3, 1, 5, 2, 4}, 5, 3)},
-                {new TestDataForGame(new int[]{4, 3, 5, 2, 1}, 5, 4)},
+                {new TestDataForGame("Removed: 1 2 3 4, Winner: 5", 5, 1)},
+                {new TestDataForGame("Removed: 2 4 1 5, Winner: 3", 5, 2)},
+                {new TestDataForGame("Removed: 3 1 5 2, Winner: 4", 5, 3)},
+                {new TestDataForGame("Removed: 4 3 5 2, Winner: 1", 5, 4)},
+                {new TestDataForGame("Removed: 5 1 3 4, Winner: 2", 5, 5)},
 
-                {new TestDataForGame(new int[]{5, 10, 6, 2, 9, 8, 1, 4, 7, 3}, 10, 5)},
-                {new TestDataForGame(new int[]{1, 3, 6, 10, 8, 9, 5, 2, 4, 7}, 10, 11)},
-                {new TestDataForGame(new int[]{10, 2, 6, 4, 7, 5, 3, 9, 8, 1}, 10, 20)},
-                {new TestDataForGame(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 1)},
+                {new TestDataForGame("Removed: 5 10 6 2 9 8 1 4 7, Winner: 3", 10, 5)},
+                {new TestDataForGame("Removed: 1 3 6 10 8 9 5 2 4, Winner: 7", 10, 11)},
+                {new TestDataForGame("Removed: 10 2 6 4 7 5 3 9 8, Winner: 1", 10, 20)},
+                {new TestDataForGame("Removed: 1 2 3 4 5 6 7 8 9, Winner: 10", 10, 1)},
 
-                {new TestDataForGame(new int[]{1, 2}, 2, 1)},
-                {new TestDataForGame(new int[]{2, 1}, 2, 2)},
-
-                {new TestDataForGame(new int[]{1}, 1, 1)},
+                {new TestDataForGame("Removed: 1, Winner: 2", 2, 1)},
+                {new TestDataForGame("Removed: 2, Winner: 1", 2, 2)},
         };
     }
 
 
     @Test(dataProvider = "getData", priority = 1)
     public void testPrintSequenceAndWinner(TestDataForGame testDataForGame) throws InvalidChildrenGameInputException {
-        Assert.assertEquals(testDataForGame.result, Game.printSequenceAndWinner(testDataForGame.numOfChildren, testDataForGame.removeAt).toArray());
+        Assert.assertEquals(Game.printSequenceAndWinner(testDataForGame.numOfChildren, testDataForGame.removeAt), testDataForGame.result);
     }
 
     private final class TestDataForGame {
-        private int[] result;
+        private String result;
         private int numOfChildren;
         private int removeAt;
 
-        public TestDataForGame(int[] result, int numOfChildren, int removeAt) {
+        public TestDataForGame(String result, int numOfChildren, int removeAt) {
             this.result = result;
             this.numOfChildren = numOfChildren;
             this.removeAt = removeAt;
